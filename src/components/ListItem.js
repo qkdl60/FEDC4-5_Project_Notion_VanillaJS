ac1b6d06488a3bd2046166e2a1b942388d05081f
nocheol <qkdl60@gmail.com>
2024-03-30 14:31:37 +0900

    refactor: details 태그 적용

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -20,12 +20,14 @@ export default class ListItem extends HTMLElement {
   // TODO 아이템 포인터, 배경 변경, 각 버튼 호버시 설명 넣기
   template(state) {
     return `
-    <div style="display:flex; flex-direction:row-reverse">
-      <div style="flex-grow:1"  class="list list-item">${state.title} <button class="list  list-item__button--add">+</button> <button class="list list-item__button--delete">-</button></div>
-      <details style="padding: 0 24px;" id="item${state.id}">
+    <div style="display:flex; flex-direction:row">
+      <details style="padding:0 16px 0 24px; position:relative; width:10px; overflow: visible; white-space:nowrap; " >
         <summary></summary>
+        <div id="item${state.id}" style="position:absolute left: 0" >
         ${state.isLast ? "더 이상 문서가 없습니다." : ""}
+        </div>
       </details>
+      <div  class="list list-item">${state.title} <button class="list  list-item__button--add">+</button> <button class="list list-item__button--delete">-</button></div>
     </div>`;
   }
 
