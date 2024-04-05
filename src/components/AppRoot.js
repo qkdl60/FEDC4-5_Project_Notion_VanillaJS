588ba86612750e0430a17620610bb022a0ebe77e
nocheol <qkdl60@gmail.com>
2024-04-05 23:11:59 +0900

    fix: add-error

--- src/components/AppRoot.js
+++ src/components/AppRoot.js
@@ -24,6 +24,7 @@ export default class AppRoot extends HTMLElement {
       this.documentId = id;
       const $listItem = document.getElementById(this.documentId);
       this.$editorPage.documentId = this.documentId;
+      console.log($listItem);
       this.$editorPage.title = $listItem.title;
     });
 
