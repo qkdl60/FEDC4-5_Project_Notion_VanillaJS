125e76d2cb8026abf92810513323206fcd262620
nocheol <qkdl60@gmail.com>
2024-04-13 17:30:31 +0900

    refactor:  클래스 방식 적용

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-import AppRoot from "./src/components/AppRoot.js";
-import EditorPage from "./src/components/EditorPage.js";
-import ListPage from "./src/components/ListPage.js";
-import ListItem from "./src/components/ListItem.js";
+import App from "./src/components/App.js";
 
-(function defineElements() {
-  window.customElements.define("app-root", AppRoot);
-  window.customElements.define("list-page", ListPage);
-  window.customElements.define("editor-page", EditorPage);
-  window.customElements.define("list-item", ListItem);
-})();
+new App(document.getElementById("app"));
