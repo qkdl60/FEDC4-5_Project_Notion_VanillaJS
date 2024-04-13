a103c28789fd6f345aacc46c34d93be508627a61
nocheol <qkdl60@gmail.com>
2024-04-13 17:29:23 +0900

    re

--- src/components/EditorPage.js
+++ src/components/EditorPage.js
@@ -1,73 +1,19 @@
-import { getDocumentContent } from "../utils/api.js";
-
-// TODO 이벤트 관리도 필요, 브래드크럼을 위한 헤더 필요
-const updateDocumentEvent = (id, title, content) =>
-  new CustomEvent("update_document", { detail: { id, title, content } });
-
-export default class EditorPage extends HTMLElement {
-  constructor() {
-    super();
-    this.content = "content 값";
-    this.addEventListener("input", (event) => {
-      const $content = this.querySelector(".editor--content");
-      const $title = this.querySelector(".editor--title");
-      const contentValue = $content.innerText;
-      const titleValue = $title.innerText;
-      window.dispatchEvent(
-        updateDocumentEvent(this.documentId, titleValue, contentValue),
-      );
-    });
-  }
-
-  get documentId() {
-    return this.getAttribute("document-id") || "";
-  }
-
-  set documentId(value) {
-    this.setAttribute("document-id", value);
-  }
-
-  get title() {
-    return this.getAttribute("title") || "";
-  }
-
-  set title(value) {
-    this.setAttribute("title", value);
-  }
-
-  static get observedAttributes() {
-    return ["document-id", "title"];
-  }
-
-  async attributeChangedCallback(attr, oldValue, newValue) {
-    if (oldValue === newValue) return;
-    this[attr] = newValue;
-    if (attr === "document-id" && this.documentId !== "null") {
-      const { content } = await getDocumentContent(`/${this.documentId}`);
-      this.content = content;
-    }
-    this.render();
-  }
-
-  async connectedCallback() {
-    this.render();
-  }
-
-  // TODO 브레드크럼 적용
+import Component from "../core/Component.js";
+// TODO 초기값은 플레이스 홀드 스타일, id undefined 처리
+export default class EditorPage extends Component {
   template() {
     return `
   <div class='editor-page__header'>
     브래드크럼,전역 상태가 필요한 부분
   </div> 
-  <div class="editor-page__body" >
-    <div contentEditable=${this.documentId !== "null"} class="editor--title" >${this.title}</div>
-    <div contentEditable=${this.documentId !== "null"} class="editor--content" > ${this.content}</div>
+  <div id="content-${this.props.id}" class="editor-page__body" >
+    <div contentEditable=${!!this.props.id} class="editor--title" >
+    ${this.props.title ? `<span id="title">${this.props.title}</span>` : "초기 페이지입니다. "}
+    </div>
+    <div contentEditable=${!!this.props.id} class="editor--content" >
+    ${this.props.content ? `<span id="content">${this.props.content}</span` : ""}
+    </div>
   </div>
-  
     `;
-  }
-
-  render() {
-    this.innerHTML = this.template();
   }
 }
