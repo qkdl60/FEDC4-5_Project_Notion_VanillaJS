d8ee50452ab4deab7b97b5c2e24688a2e1402e35
nocheol <qkdl60@gmail.com>
2024-04-13 17:28:09 +0900

    chore: 웹컴포넌트 제거

---
+++ src/web-components/ListItem.js
@@ -0,0 +1,86 @@
+export default class ListItem extends HTMLElement {
+  get id() {
+    return this.getAttribute("id") || "";
+  }
+
+  set id(value) {
+    this.setAttribute("id", value);
+  }
+
+  get title() {
+    return this.getAttribute("title") || "";
+  }
+
+  set title(value) {
+    this.setAttribute("title", value);
+  }
+
+  get isOpen() {
+    return this.getAttribute("is-open") === "true";
+  }
+
+  set isOpen(value) {
+    this.setAttribute("is-open", value);
+  }
+
+  get childDocuments() {
+    const returnValue = this.getAttribute("child-documents");
+    return returnValue ? JSON.parse(returnValue) : [];
+  }
+
+  set childDocuments(value) {
+    this.setAttribute("child-documents", value);
+  }
+
+  static get observedAttributes() {
+    return ["title", "is-open", "child-documents"];
+  }
+
+  async attributeChangedCallback(attr, oldValue, newValue) {
+    if (oldValue === newValue) return;
+    this[attr] = newValue;
+    if (attr === "is-open") return;
+    this.render();
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  // TODO 아이템 포인터, 배경 변경, 각 버튼 호버시 설명 넣기, 스타일 css로 변경, details 변경 필요(상태로 컨트롤이 안된다. )
+  template() {
+    return `
+    <div class="list-item__container" >
+      <details id="details-${this.id}"  ${this.isOpen ? "open" : ""}>
+        <summary></summary>
+        <div class="list-item__child" id="item${this.id}" >
+        ${
+          this.childDocuments.length
+            ? this.childDocuments
+                .map((doc) => {
+                  return `<list-item id=${doc.id} title=${doc.title} is-open=${doc.isOpen}></list-item>`;
+                })
+                .join("")
+            : "<p class='list-item__child--empty-text' >하위 문서 없음</p>"
+        }
+        </div>
+      </details>
+      <div  class="list list-item list-item__title">
+        <div class="list list-item list-item__title--text">
+        ${this.title}
+        </div>
+        <div class="list-item__title--buttons">
+        <button class="list list-item__button--delete">-</button> <button class="list list-item__button--add">+</button> 
+        </div>
+      </div>
+    </div>`;
+  }
+
+  render() {
+    this.innerHTML = this.template();
+    this.childDocuments.forEach((doc) => {
+      const $listItem = document.getElementById(doc.id);
+      if ($listItem) $listItem.childDocuments = JSON.stringify(doc.documents);
+    });
+  }
+}
---
+++ src/web-components/ListPage.js
@@ -0,0 +1,116 @@
+import { IS_OPEN_STATE_LIST_KEY } from "../constant/constant.js";
+import { createDocument, deleteDocument } from "../utils/api.js";
+import { push } from "../utils/router.js";
+import { setItem } from "../utils/storage.js";
+
+const eventCreateDocumentsTree = new CustomEvent("createDocumentsTree");
+// TODO openList 관리 필요
+export default class ListPage extends HTMLElement {
+  constructor() {
+    super();
+    this.$list = null;
+    this.isOpenList = new Set();
+    this.addEventListener("click", async (event) => {
+      const { target } = event;
+
+      if (target.tagName === "SUMMARY") {
+        const $details = target.closest("details");
+        const $listItem = target.closest("list-item");
+        $listItem.isOpen = $details.getAttribute("open") === null;
+        const { id } = $listItem;
+        if ($listItem.isOpen) {
+          this.isOpenList.add(id);
+          setItem(IS_OPEN_STATE_LIST_KEY, [...this.isOpenList]);
+        } else if ($listItem.isOpen === false && this.isOpenList.has(id)) {
+          this.isOpenList.delete(id);
+          setItem(IS_OPEN_STATE_LIST_KEY, [...this.isOpenList]);
+        }
+        return;
+      }
+
+      const targetClassList = target.classList;
+
+      if (
+        targetClassList !== undefined &&
+        targetClassList.contains("button--root-add")
+      ) {
+        await createDocument("제목없음");
+        window.dispatchEvent(eventCreateDocumentsTree);
+      } else if (
+        targetClassList !== undefined &&
+        targetClassList.contains("list")
+      ) {
+        const targetItem = target.closest("list-item");
+        const targetItemId = targetItem.id;
+        if (targetClassList.contains("list-item__button--add")) {
+          const created = await createDocument("제목없음", targetItemId);
+          targetItem.isOpen = true;
+          this.isOpenList.add(targetItemId);
+          setItem(IS_OPEN_STATE_LIST_KEY, [...this.isOpenList]);
+          window.dispatchEvent(eventCreateDocumentsTree);
+
+          // TODO 에러 처리,
+        } else if (targetClassList.contains("list-item__button--delete")) {
+          await deleteDocument(`/${targetItemId}`);
+          if (window.dispatchEvent(eventCreateDocumentsTree)) push();
+        } else {
+          push(targetItemId);
+        }
+      }
+    });
+  }
+
+  get list() {
+    return JSON.parse(this.getAttribute("list")) || [];
+  }
+
+  set list(nextList) {
+    this.setAttribute("list", nextList);
+  }
+
+  static get observedAttributes() {
+    return ["list"];
+  }
+
+  async attributeChangedCallback(attr, oldValue, newValue) {
+    if (oldValue === newValue) return;
+    this.list = newValue;
+    this.render();
+  }
+
+  async disconnectedCallback() {
+    setItem(IS_OPEN_STATE_LIST_KEY, [...this.isOpenList]);
+  }
+
+  async connectedCallback() {
+    this.render();
+
+    // TODO 로딩 처리필요, 스켈레톤으로 구현,
+  }
+
+  template() {
+    return `
+    <div class="list-page__header"><span class="list-page__header--text">개인 문서</span> <button class="button--root-add">+</button></div>
+    <div class="document-list">
+    </div>
+`;
+  }
+
+  render() {
+    this.innerHTML = this.template();
+    this.$list = this.querySelector(".document-list");
+    this._renderDocumentsTree();
+  }
+
+  _renderDocumentsTree() {
+    if (!this.list || this.list.length === 0) return;
+    this.list.forEach((item) => {
+      const $listItem = document.createElement("list-item");
+      $listItem.id = item.id;
+      $listItem.title = item.title;
+      $listItem.childDocuments = JSON.stringify(item.documents);
+      $listItem.isOpen = item.isOpen;
+      this.$list.appendChild($listItem);
+    });
+  }
+}
