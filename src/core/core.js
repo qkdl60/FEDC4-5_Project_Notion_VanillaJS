da8213d5ff7bbe296fb4fdee7bb3d1dac3ca8b78
nocheol <qkdl60@gmail.com>
2024-09-15 17:54:55 +0900

    feat : 함수형 컴포넌트 렌더링 기능 추가

--- src/core/core.js
+++ src/core/core.js
@@ -18,6 +18,12 @@ export function createElement(type, props, ...children) {
 export function render(element, container) {
   const { type, props, children } = element;
   let $el;
+  // 함수형 컴포넌트 렌더링
+  if (typeof type === "function") {
+    const resultEl = type(props);
+    render(resultEl, container);
+    return;
+  }
   if (type === "text") {
     $el = document.createTextNode(props.value);
   } else {
